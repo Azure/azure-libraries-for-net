3343bb7dd8798dcf3566e06b000bf4e955ff8381
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-09-19 16:40:08 -0700

    peering+watcher sample

--- src/ResourceManagement/ResourceManager/SdkContext.cs
+++ src/ResourceManagement/ResourceManager/SdkContext.cs
@@ -15,6 +15,24 @@ namespace Microsoft.Azure.Management.ResourceManager.Fluent
 
         public static DelayProvider DelayProvider { get; set; } = new DelayProvider();
 
+        /// <summary>
+        /// Generates the specified number of random resource names with the same prefix.
+        /// </summary>
+        /// <param name="prefix">the prefix to be used if possible</param>
+        /// <param name="maxLen">the maximum length for the random generated name</param>
+        /// <param name="count">the number of names to generate</param>
+        /// <returns>random names</returns>
+        public static string[] RandomResourceNames(string prefix, int maxLen, int count)
+        {
+            string[] names = new string[count];
+            var resourceNamer = SdkContext.CreateResourceNamer("");
+            for (int i = 0; i < count; i++)
+            {
+                names[i] = resourceNamer.RandomName(prefix, maxLen);
+            }
+            return names;
+        }
+
         public static string RandomResourceName(string prefix, int maxLen)
         {
             var namer = CreateResourceNamer("");
