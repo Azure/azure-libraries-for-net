0668d5fd0daf42a25aee03ae630c2d6e18cfbd6b
Hovsep <hovsepm@microsoft.com>
2017-09-19 17:22:24 -0700

    Added Generated Sync method and License Header check on Build. (#11)
    * Added Generated Sync method and License Header check on Build.
    
    * Fixed License Headers for existing files.

--- Tests/Fluent.Tests/Dns/DnsRecordSetTests.cs
+++ Tests/Fluent.Tests/Dns/DnsRecordSetTests.cs
@@ -1,7 +1,7 @@
-﻿using Azure.Tests;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Azure.Tests;
 using Fluent.Tests.Common;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 using Microsoft.Rest.Azure;
