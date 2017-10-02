4e83d285f19a428ed4f7d18170e57212f7e15c27
Anu Thomas Chandy <anuamd@hotmail.com>
2017-10-02 00:14:51 -0700

    addressing zonal VMSS sample offline review comments

--- Samples/Compute/ManageZonalVirtualMachineScaleSet.cs
+++ Samples/Compute/ManageZonalVirtualMachineScaleSet.cs
@@ -13,7 +13,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ManageZoneRedundantVirtualMachineScaleSet
+namespace ManageZonalVirtualMachineScaleSet
 {
     public class Program
     {
