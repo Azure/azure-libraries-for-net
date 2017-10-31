5a027924a47a02ea61124aa5dce875a65db03d68
Anu Thomas Chandy <anuamd@hotmail.com>
2017-10-31 12:43:24 -0700

    deprecating obsolete VM images

--- src/ResourceManagement/Compute/KnownWindowsVirtualMachineImage.cs
+++ src/ResourceManagement/Compute/KnownWindowsVirtualMachineImage.cs
@@ -9,12 +9,18 @@ namespace Microsoft.Azure.Management.Compute.Fluent
     {
         [EnumName("MicrosoftWindowsServer WindowsServer 2008-R2-SP1")]
         WindowsServer2008R2_SP1,
+
         [EnumName("MicrosoftWindowsServer WindowsServer 2012-Datacenter")]
         WindowsServer2012Datacenter,
+
         [EnumName("MicrosoftWindowsServer WindowsServer 2012-R2-Datacenter")]
         WindowsServer2012R2Datacenter,
+
+        [System.Obsolete("For virtual machine and scale set, use withLatestLinuxImage(String publisher, String offer, String sku) with publisher as 'MicrosoftWindowsServer', offer as 'WindowsServer' and sku as '2016-Datacenter-with-Containers'")]
         [EnumName("MicrosoftWindowsServer WindowsServer 2016-Technical-Preview-with-Containers")]
         WindowsServer2016TechnicalPreviewWithContainers,
+
+        [System.Obsolete("For virtual machine and scale set, use withLatestLinuxImage(String publisher, String offer, String sku) with publisher as 'MicrosoftWindowsServer', offer as 'WindowsServer' and sku as '2016-Datacenter'")]
         [EnumName("MicrosoftWindowsServer WindowsServer Windows-Server-Technical-Preview")]
         WindowsServerTechnicalPreview
     }
