2e1b906d33407552ae64f18d7a878afa9fbe6ddb
Rikki Gibson <rigibson@microsoft.com>
2018-02-05 11:12:07 -0800

    Set csharp.clear-output-folder=false. Remove redundant path component.

--- src/ResourceManagement/gulpfile.js
+++ src/ResourceManagement/gulpfile.js
@@ -82,7 +82,7 @@ var handleInput = function (projects, cb) {
 
 var codegen = function (project, cb) {
     const regenManager = args['regenerate-manager'] ? ' --regenerate-manager=true ' : '';
-    var outputDir = path.resolve(mappings[project].dir) + '/Generated';
+    var outputDir = path.resolve(mappings[project].dir);
     if (!args['preserve']) {
         deleteFolderRecursive(outputDir);
     }
@@ -107,6 +107,7 @@ var codegen = function (project, cb) {
         ` --csharp.namespace=${mappings[project].package} ` +
         ` --csharp.output-folder=${outputDir} ` +
         ` --csharp.license-header=MICROSOFT_MIT_NO_CODEGEN ` +
+        ` --csharp.clear-output-folder=false ` +
         autorestUseArg +
         regenManager +
         ' --package-version=1.3.0 ' +
