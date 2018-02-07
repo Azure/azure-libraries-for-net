7b254e97bc88af0e091d099e586e5b8d59bb76b5
lenala <lena.lakhno@gmail.com>
2018-02-07 14:09:35 -0800

    license header

--- Samples/BatchAI/ManageBatchAI.cs
+++ Samples/BatchAI/ManageBatchAI.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Azure.Management.BatchAI.Fluent;
 using Microsoft.Azure.Management.BatchAI.Fluent.Models;
 using Microsoft.Azure.Management.Compute.Fluent.Models;
@@ -53,8 +55,8 @@ namespace ManageBatchAI
 
                 StorageAccountKey storageAccountKey = storageAccount.GetKeys().First();
 
-                CloudFileShare cloudFileShare = CloudStorageAccount.Parse(
-                        $"DefaultEndpointsProtocol=https;AccountName={saName};AccountKey={storageAccountKey};EndpointSuffix=core.windows.net")
+                CloudFileShare cloudFileShare = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=" + saName + ";AccountKey="+ storageAccountKey.Value +
+                    ";EndpointSuffix=core.windows.net")
                     .CreateCloudFileClient()
                     .GetShareReference(shareName);
                 cloudFileShare.CreateAsync().GetAwaiter().GetResult();
