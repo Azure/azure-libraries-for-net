9a9e3ed10d772fee201ab5781e8286a82b28d3ae
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-02-23 15:29:15 -0800

    Add missing license header

--- src/ResourceManagement/AppService/KuduClient.cs
+++ src/ResourceManagement/AppService/KuduClient.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Rest;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+using Microsoft.Rest;
 using Microsoft.Rest.Azure;
 using Newtonsoft.Json;
 using System;
