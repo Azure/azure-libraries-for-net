9d972e8e88d6b77e36055ce7ec2619917d89ad21
Anu Thomas Chandy <anuamd@hotmail.com>
2018-02-27 14:04:37 -0800

    Event hub samples (manage basic eventhub, associated resources and geo-disaster recovery)

---
+++ Tests/Samples.Tests/EventHub.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Azure.Tests;
+using Fluent.Tests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Samples.Tests
+{
+    public class EventHub : Samples.Tests.TestBase
+    {
+        public EventHub(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        [Trait("Samples", "EventHub")]
+        public void ManageEventHubTest()
+        {
+            using (var context = FluentMockContext.Start(this.GetType().FullName))
+            {
+                var rollUpClient = TestHelper.CreateRollupClient();
+                ManageEventHub.Program.RunSample(rollUpClient);
+            }
+        }
+
+        [Fact]
+        [Trait("Samples", "EventHub")]
+        public void ManageEventHubGeoDisasterRecoveryTest()
+        {
+            using (var context = FluentMockContext.Start(this.GetType().FullName))
+            {
+                var rollUpClient = TestHelper.CreateRollupClient();
+                ManageEventHub.Program.RunSample(rollUpClient);
+            }
+        }
+    }
+}
