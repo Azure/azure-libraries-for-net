a9c905512824e8b70f6d408a5ac365a1408c9381
lenala <lena.lakhno@gmail.com>
2018-03-14 12:23:12 -0700

    image update for CI

---
+++ ci/get_tasks_list.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+base=`dirname {BASH_SOURCE[0]}`
+rootdir="$( cd "$base" && pwd )"
+
+echo $rootdir
+
+sampleTestPath=/azure-libraries-for-net/Tests/Samples.Tests/
+fluentTestPath=/azure-libraries-for-net/Tests/Fluent.Tests/
+
+cd $rootdir/Tests/Samples.Tests/
+
+dotnet test -t --no-build > $rootdir/ci/tasklist
+
+echo "[" > $rootdir/ci/app/test_index
+while read line; do
+    testcase="$( echo "$line" | cut -d '=' -f 1 )"
+    if [[ $testcase == Samples* ]] ; then
+        sed -e "s;%testcase%;${testcase};g" -e "s|%testpath%|${sampleTestPath}|g" $rootdir/ci/task_template > $rootdir/ci/file.out
+        task=$(cat $rootdir/ci/file.out)
+        echo $task, >> $rootdir/ci/app/test_index
+    fi
+done < $rootdir/ci/ci/tasklist
+
+cd $rootdir/Tests/Fluent.Tests/
+
+dotnet test -t --no-build > $rootdir/ci/tasklist
+
+while read line; do
+    testcase="$( echo "$line" | cut -d '=' -f 1 )"
+    if [[ $testcase == Fluent* ]] ; then
+        sed -e "s/%testcase%/${testcase}/g" -e "s|%testpath%|${fluentTestPath}|g" $rootdir/ci/ci/task_template > $rootdir/ci/file.out
+        task=$(cat $rootdir/ci/file.out)
+        echo $task, >> $rootdir/ci/app/test_index
+    fi
+done < $rootdir/ci/tasklist
+
+#sed -i '$ s|.$||' ./app/test_index
+
+truncate -s-1 $rootdir/ci/app/test_index
+
+#sed -i '$! { P; D; }; s|.$||' ./app/test_index
+
+echo "]" >> $rootdir/ci/app/test_index
