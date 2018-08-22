cff6edc500e3f53bfdfdacb685a4b985bfcd5f78
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2018-08-22 16:11:37 -0700

    Removed local Nuget feed dependency.

--- src/ResourceManagement/Search/Generated/Models/SearchServiceInner.cs
+++ src/ResourceManagement/Search/Generated/Models/SearchServiceInner.cs
@@ -189,7 +189,7 @@ namespace Microsoft.Azure.Management.Search.Fluent.Models
         /// <exception cref="ValidationException">
         /// Thrown if validation fails
         /// </exception>
-        public virtual void Validate()
+        public override void Validate()
         {
             if (Sku == null)
             {
