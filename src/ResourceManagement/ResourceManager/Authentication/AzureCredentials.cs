006b9368dc647fcecb1660f7b27d2e3a3ae072c1
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2018-11-05 11:00:08 -0800

    fixed build

--- src/ResourceManagement/ResourceManager/Authentication/AzureCredentials.cs
+++ src/ResourceManagement/ResourceManager/Authentication/AzureCredentials.cs
@@ -6,7 +6,9 @@ using Microsoft.Rest;
 using Microsoft.Rest.Azure.Authentication;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
