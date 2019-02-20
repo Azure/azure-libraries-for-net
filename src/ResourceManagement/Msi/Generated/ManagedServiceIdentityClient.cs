ff13582f8e5ccec52eb5f6b3a8ec73a142fbe576
Pareekshit Singh <pareekshit.singh@microsoft.com>
2019-02-19 16:20:10 -0800

    Fix for issue #591 Updating the MSI dependency to the l;atest stable version

--- src/ResourceManagement/Msi/Generated/ManagedServiceIdentityClient.cs
+++ src/ResourceManagement/Msi/Generated/ManagedServiceIdentityClient.cs
@@ -34,7 +34,7 @@ namespace Microsoft.Azure.Management.Msi.Fluent
         /// Gets or sets json deserialization settings.
         /// </summary>
         public JsonSerializerSettings DeserializationSettings { get; private set; }
-        
+
         /// <summary>
         /// The Id of the Subscription to which the identity belongs.
         /// </summary>
@@ -46,19 +46,20 @@ namespace Microsoft.Azure.Management.Msi.Fluent
         public string ApiVersion { get; private set; }
 
         /// <summary>
-        /// Gets or sets the preferred language for the response.
+        /// The preferred language for the response.
         /// </summary>
         public string AcceptLanguage { get; set; }
 
         /// <summary>
-        /// Gets or sets the retry timeout in seconds for Long Running Operations.
-        /// Default value is 30.
+        /// The retry timeout in seconds for Long Running Operations. Default value is
+        /// 30.
         /// </summary>
         public int? LongRunningOperationRetryTimeout { get; set; }
 
         /// <summary>
-        /// When set to true a unique x-ms-client-request-id value is generated and
-        /// included in each request. Default is true.
+        /// Whether a unique x-ms-client-request-id should be generated. When set to
+        /// true a unique x-ms-client-request-id value is generated and included in
+        /// each request. Default is true.
         /// </summary>
         public bool? GenerateClientRequestId { get; set; }
 
@@ -95,7 +96,7 @@ namespace Microsoft.Azure.Management.Msi.Fluent
             Operations = new Operations(this);
             UserAssignedIdentities = new UserAssignedIdentitiesOperations(this);
             BaseUri = new System.Uri("https://management.azure.com");
-            ApiVersion = "2015-08-31-preview";
+            ApiVersion = "2018-11-30";
             AcceptLanguage = "en-US";
             LongRunningOperationRetryTimeout = 30;
             GenerateClientRequestId = true;
