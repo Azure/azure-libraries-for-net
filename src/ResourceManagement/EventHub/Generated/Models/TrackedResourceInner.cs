239d3638350e765a030e2648b45dbd10666f0d7a
Yaohai Zheng <yaozheng@microsoft.com>
2019-06-20 12:30:28 +0800

    Fix tests.

--- src/ResourceManagement/EventHub/Generated/Models/TrackedResourceInner.cs
+++ src/ResourceManagement/EventHub/Generated/Models/TrackedResourceInner.cs
@@ -34,10 +34,8 @@ namespace Microsoft.Azure.Management.EventHub.Fluent.Models
         /// <param name="location">Resource location</param>
         /// <param name="tags">Resource tags</param>
         public TrackedResourceInner(string id = default(string), string name = default(string), string type = default(string), string location = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>))
-            : base(id, name, type)
+            : base(location, id, name, type, tags)
         {
-            Location = location;
-            Tags = tags;
             CustomInit();
         }
 
@@ -45,18 +43,6 @@ namespace Microsoft.Azure.Management.EventHub.Fluent.Models
         /// An initialization method that performs custom operations like setting defaults
         /// </summary>
         partial void CustomInit();
-
-        /// <summary>
-        /// Gets or sets resource location
-        /// </summary>
-        [JsonProperty(PropertyName = "location")]
-        public string Location { get; set; }
-
-        /// <summary>
-        /// Gets or sets resource tags
-        /// </summary>
-        [JsonProperty(PropertyName = "tags")]
-        public IDictionary<string, string> Tags { get; set; }
 
     }
 }
