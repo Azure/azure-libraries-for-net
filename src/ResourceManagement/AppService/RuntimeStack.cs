fd469ed612b297b7f767a6a6ff309075108c6c2e
Arian Celina <arian.celina@gmail.com>
2019-08-26 10:15:02 +0200

    Versions corrected
    Versions listed through ```az webapp list-runtimes --linux```

--- src/ResourceManagement/AppService/RuntimeStack.cs
+++ src/ResourceManagement/AppService/RuntimeStack.cs
@@ -7,8 +7,10 @@ namespace Microsoft.Azure.Management.AppService.Fluent
     /// </summary>
     public partial class RuntimeStack
     {
-        public static readonly RuntimeStack NodeJS_LTS = new RuntimeStack("NODE", "LTS");
-        public static readonly RuntimeStack NodeJS_10_LTS = new RuntimeStack("NODE", "10 LTS");
+        public static readonly RuntimeStack NodeJS_LTS = new RuntimeStack("NODE", "lts");
+        public static readonly RuntimeStack NodeJS_10_LTS = new RuntimeStack("NODE", "10-lts");
+        public static readonly RuntimeStack NodeJS_8_LTS = new RuntimeStack("NODE", "8-lts");
+        public static readonly RuntimeStack NodeJS_6_LTS = new RuntimeStack("NODE", "6-lts");
         public static readonly RuntimeStack NodeJS_10_14 = new RuntimeStack("NODE", "10.14");
         public static readonly RuntimeStack NodeJS_10_12 = new RuntimeStack("NODE", "10.12");
         public static readonly RuntimeStack NodeJS_10_10 = new RuntimeStack("NODE", "10.10");
@@ -37,16 +39,19 @@ namespace Microsoft.Azure.Management.AppService.Fluent
         public static readonly RuntimeStack NETCore_V2_0 = new RuntimeStack("DOTNETCORE", "2.0");
         public static readonly RuntimeStack NETCore_V2_1 = new RuntimeStack("DOTNETCORE", "2.1");
         public static readonly RuntimeStack NETCore_V2_2 = new RuntimeStack("DOTNETCORE", "2.2");
-        public static readonly RuntimeStack Ruby_2_3 = new RuntimeStack("RUBY", "2.3");
-        public static readonly RuntimeStack Ruby_2_4 = new RuntimeStack("RUBY", "2.4");
-        public static readonly RuntimeStack Ruby_2_5 = new RuntimeStack("RUBY", "2.5");
-        public static readonly RuntimeStack Ruby_2_6 = new RuntimeStack("RUBY", "2.6");
+        public static readonly RuntimeStack Ruby_2_3 = new RuntimeStack("RUBY", "2.3.8");
+        public static readonly RuntimeStack Ruby_2_4 = new RuntimeStack("RUBY", "2.4.5");
+        public static readonly RuntimeStack Ruby_2_5 = new RuntimeStack("RUBY", "2.5.5");
+        public static readonly RuntimeStack Ruby_2_6 = new RuntimeStack("RUBY", "2.6.2");
         public static readonly RuntimeStack Java_8_Jre8 = new RuntimeStack("JAVA", "8-jre8");
         public static readonly RuntimeStack Java_11_Java11 = new RuntimeStack("JAVA", "11-java11");
         public static readonly RuntimeStack Tomcat_8_5_JRE8 = new RuntimeStack("TOMCAT", "8.5-jre8");
         public static readonly RuntimeStack Tomcat_8_5_JAVA11 = new RuntimeStack("TOMCAT", "8.5-java11");
         public static readonly RuntimeStack Tomcat_9_0_JRE8 = new RuntimeStack("TOMCAT", "9.0-jre8");
         public static readonly RuntimeStack Tomcat_9_0_JAVA11 = new RuntimeStack("TOMCAT", "9.0-java11");
+        public static readonly RuntimeStack Python_2_7 = new RuntimeStack("PYTHON", "2.7");
+        public static readonly RuntimeStack Python_3_6 = new RuntimeStack("PYTHON", "3.6");
+        public static readonly RuntimeStack Python_3_7 = new RuntimeStack("PYTHON", "3.7");
 
         private string stack;
         private string version;
