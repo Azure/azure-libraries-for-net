7e89e7581a79d11868b73accfea7cf474aae22b0
Weidong Xu <weidxu@microsoft.com>
2019-09-30 14:04:27 +0800

    fix compile

--- src/ResourceManagement/AppService/Domain/IFunctionApps.cs
+++ src/ResourceManagement/AppService/Domain/IFunctionApps.cs
@@ -5,6 +5,8 @@ namespace Microsoft.Azure.Management.AppService.Fluent
     using Microsoft.Azure.Management.AppService.Fluent.FunctionApp.Definition;
     using Microsoft.Azure.Management.ResourceManager.Fluent.Core.CollectionActions;
     using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+    using System.Threading.Tasks;
+    using System.Threading;
 
     /// <summary>
     /// Entry point for web app management API.
@@ -19,8 +21,42 @@ namespace Microsoft.Azure.Management.AppService.Fluent
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.CollectionActions.ISupportsGettingById<Microsoft.Azure.Management.AppService.Fluent.IFunctionApp>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.CollectionActions.ISupportsDeletingByResourceGroup,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IHasManager<Microsoft.Azure.Management.AppService.Fluent.IAppServiceManager>,
-        Microsoft.Azure.Management.ResourceManager.Fluent.Core.IHasInner<Microsoft.Azure.Management.AppService.Fluent.IWebAppsOperations>,
-        ISupportAppServiceDelete
+        Microsoft.Azure.Management.ResourceManager.Fluent.Core.IHasInner<Microsoft.Azure.Management.AppService.Fluent.IWebAppsOperations>
     {
+        /// <summary>
+        /// Deletes a resource from Azure, identifying it by its resource ID.
+        /// </summary>
+        /// <param name="id">the resource ID of the resource to delete</param>
+        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
+        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
+        void DeleteById(string id, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?));
+
+        /// <summary>
+        /// Deletes a resource from Azure, identifying it by its resource ID.
+        /// </summary>
+        /// <param name="id">the resource ID of the resource to delete</param>
+        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
+        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
+        /// <param name="cancellationToken">cancellationToken the cancellation token</param>
+        Task DeleteByIdAsync(string id, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?), CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Deletes a resource from Azure, identifying it by its name and its resource group.
+        /// </summary>
+        /// <param name="resourceGroupName">the group the resource is part of</param>
+        /// <param name="name">the name of the resource</param>
+        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
+        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
+        void DeleteByResourceGroup(string resourceGroupName, string name, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?));
+
+        /// <summary>
+        /// Deletes a resource from Azure, identifying it by its name and its resource group.
+        /// </summary>
+        /// <param name="resourceGroupName">the group the resource is part of</param>
+        /// <param name="name">the name of the resource</param>
+        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
+        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
+        /// <param name="cancellationToken">cancellationToken the cancellation token</param>
+        Task DeleteByResourceGroupAsync(string resourceGroupName, string name, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?), CancellationToken cancellationToken = default(CancellationToken));
     }
 }
--- src/ResourceManagement/AppService/Domain/IWebApp.cs
+++ src/ResourceManagement/AppService/Domain/IWebApp.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Azure.Management.AppService.Fluent
     /// <summary>
     /// An immutable client-side representation of an Azure Web App.
     /// </summary>
-    public interface IWebApp  :
+    public interface IWebApp :
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IBeta,
         Microsoft.Azure.Management.AppService.Fluent.IWebAppBase,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions.IRefreshable<Microsoft.Azure.Management.AppService.Fluent.IWebApp>,
@@ -34,41 +34,5 @@ namespace Microsoft.Azure.Management.AppService.Fluent
         /// </summary>
         /// <param name="warFile">The WAR file to upload.</param>
         void WarDeploy(FileInfo warFile);
-
-        /// <summary>
-        /// Deletes a resource from Azure, identifying it by its resource ID.
-        /// </summary>
-        /// <param name="id">the resource ID of the resource to delete</param>
-        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
-        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
-        void DeleteById(string id, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?));
-
-        /// <summary>
-        /// Deletes a resource from Azure, identifying it by its resource ID.
-        /// </summary>
-        /// <param name="id">the resource ID of the resource to delete</param>
-        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
-        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
-        /// <param name="cancellationToken">cancellationToken the cancellation token</param>
-        Task DeleteByIdAsync(string id, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?), CancellationToken cancellationToken = default(CancellationToken));
-
-        /// <summary>
-        /// Deletes a resource from Azure, identifying it by its name and its resource group.
-        /// </summary>
-        /// <param name="resourceGroupName">the group the resource is part of</param>
-        /// <param name="name">the name of the resource</param>
-        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
-        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
-        void DeleteByResourceGroup(string resourceGroupName, string name, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?));
-
-        /// <summary>
-        /// Deletes a resource from Azure, identifying it by its name and its resource group.
-        /// </summary>
-        /// <param name="resourceGroupName">the group the resource is part of</param>
-        /// <param name="name">the name of the resource</param>
-        /// <param name="deleteMetrics">if true, web app metrics are also deleted</param>
-        /// <param name="deleteEmptyServerFarm">if true, empty App Service plan are also deleted</param>
-        /// <param name="cancellationToken">cancellationToken the cancellation token</param>
-        Task DeleteByResourceGroupAsync(string resourceGroupName, string name, bool? deleteMetrics = default(bool?), bool? deleteEmptyServerFarm = default(bool?), CancellationToken cancellationToken = default(CancellationToken));
     }
 }
