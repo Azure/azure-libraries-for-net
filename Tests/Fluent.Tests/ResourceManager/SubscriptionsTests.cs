222d58333ce93e58f5ed1e53bba2ca2b0ebdf926
Weidong Xu <weidxu@microsoft.com>
2019-10-18 12:22:42 +0800

    fix region

--- Tests/Fluent.Tests/ResourceManager/SubscriptionsTests.cs
+++ Tests/Fluent.Tests/ResourceManager/SubscriptionsTests.cs
@@ -3,10 +3,13 @@
 
 using Azure.Tests;
 using Fluent.Tests.Common;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -63,6 +66,32 @@ namespace Fluent.Tests.ResourceManager
 
                 Assert.NotNull(subscription);
                 Assert.Equal(subscription.SubscriptionId.ToLowerInvariant(), azure.SubscriptionId.ToLowerInvariant());
+            }
+        }
+
+        [Fact(Skip = "Util to generate missing regions")]
+        public void GenerateMissingRegion()
+        {
+            // Please double check generated code and make adjustment
+
+            using (var context = FluentMockContext.Start(GetType().FullName))
+            {
+                StringBuilder sb = new StringBuilder();
+                var azure = TestHelper.CreateRollupClient();
+                IEnumerable<ILocation> locations = azure.GetCurrentSubscription().ListLocations();
+                IReadOnlyDictionary<string, Region> regions = Region.Values.ToDictionary(region => region.Name);
+                foreach (ILocation location in locations)
+                {
+                    if (!regions.ContainsKey(location.Name))
+                    {
+                        sb.AppendLine().Append(String.Format(
+                            "public static readonly Region {0} = new Region(\"{1}\");",
+                            location.DisplayName.Replace(" ", ""),
+                            location.Name));
+                    }
+                }
+
+                Assert.True(sb.Length == 0, sb.ToString());
             }
         }
     }
