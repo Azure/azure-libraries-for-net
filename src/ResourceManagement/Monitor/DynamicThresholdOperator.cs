0fe7f79a8c2e65c20650388678674c0ceb4be127
Weidong Xu <weidxu@microsoft.com>
2019-10-24 17:21:55 +0800

    docstring

--- src/ResourceManagement/Monitor/DynamicThresholdOperator.cs
+++ src/ResourceManagement/Monitor/DynamicThresholdOperator.cs
@@ -4,6 +4,12 @@
 
 namespace Microsoft.Azure.Management.Monitor.Fluent.Models
 {
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The operator used to compare the metric value against the threshold.
+    /// </summary>
+    [JsonConverter(typeof(Management.ResourceManager.Fluent.Core.ExpandableStringEnumConverter<DynamicThresholdOperator>))]
     public class DynamicThresholdOperator : Management.ResourceManager.Fluent.Core.ExpandableStringEnum<DynamicThresholdOperator>
     {
         public static readonly DynamicThresholdOperator GreaterThan = Parse("GreaterThan");
