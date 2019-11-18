ec3f331b578f8a5977f7dc492120ec2d109d903d
Weidong Xu <weidxu@microsoft.com>
2019-11-18 16:02:44 +0800

    add copyright

--- tools/BuildToolsForSdk/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/BuildStages/PostBuild/GetApiTag.cs
+++ tools/BuildToolsForSdk/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/BuildStages/PostBuild/GetApiTag.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Azure.Sdk.Build.Tasks.BuildStages.PostBuild
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Sdk.Build.Tasks.BuildStages.PostBuild
 {
     /*
         using Microsoft.Azure.Sdk.Build.Tasks.BaseTasks;
--- tools/BuildToolsForSdk/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/BuildStages/PreBuild/CleanPackagesTask.cs
+++ tools/BuildToolsForSdk/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/BuildStages/PreBuild/CleanPackagesTask.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.Sdk.Build.ExecProcess;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Sdk.Build.ExecProcess;
 using Microsoft.Azure.Sdk.Build.Tasks.BaseTasks;
 using Microsoft.Build.Framework;
 using System;
