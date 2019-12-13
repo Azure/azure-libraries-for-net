30c160834ec34029f8039c7eee2cba891f3b8039
Tanyi Chen <tanchen@microsoft.com>
2019-12-13 13:31:20 +0800

    feat: add sql database with through put

---
+++ src/ResourceManagement/CosmosDB/Domain/SqlDatabase/Update/IUpdate.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Management.CosmosDB.Fluent.Domain.SqlDatabase.Update
+{
+    /// <summary>
+    /// The entirety of sql database update as a part of parent cosmos db account update.
+    /// </summary>
+    public interface IUpdate :
+        Microsoft.Azure.Management.ResourceManager.Fluent.Core.ChildResourceActions.ISettable<Microsoft.Azure.Management.CosmosDB.Fluent.CosmosDBAccount.Update.IUpdate>,
+        IWithOptions,
+        IWithThroughput
+    {
+    }
+
+    /// <summary>
+    /// The stage of the sql database update allowing to set options.
+    /// </summary>
+    public interface IWithOptions :
+        HasOptions.Update.IWithOptions<IUpdate>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the sql database update allowing to set throughput.
+    /// </summary>
+    public interface IWithThroughput :
+        HasThroughputSettings.Update.IWithThroughput<IUpdate>
+    {
+    }
+}
