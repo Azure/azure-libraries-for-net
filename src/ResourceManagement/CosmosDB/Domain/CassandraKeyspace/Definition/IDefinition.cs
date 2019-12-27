60d21e7caab2a38d644bfa7cd442af22c42e2015
Tanyi Chen <tanchen@microsoft.com>
2019-12-27 13:32:52 +0800

    feat: add cassandra interface

---
+++ src/ResourceManagement/CosmosDB/Domain/CassandraKeyspace/Definition/IDefinition.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Management.CosmosDB.Fluent.CassandraKeyspace.Definition
+{
+    /// <summary>
+    /// The entirety of a Cassandra Keyspace definition as a part of parent definition.
+    /// </summary>
+    /// <typeparam name="ParentT">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IDefinition<ParentT> :
+        IBlank<ParentT>,
+        IWithAttach<ParentT>
+    {
+    }
+
+    /// <summary>
+    /// The first stage of a Cassandra Keyspace definition.
+    /// </summary>
+    /// <typeparam name="ParentT">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IBlank<ParentT> :
+        IWithAttach<ParentT>
+    {
+    }
+
+    /// <summary>
+    /// The final stage of the Cassandra Keyspace definition.
+    /// </summary>
+    /// <typeparam name="ParentT">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IWithAttach<ParentT> :
+        Microsoft.Azure.Management.ResourceManager.Fluent.Core.ChildResource.Definition.IInDefinition<ParentT>,
+        IWithOptions<ParentT>,
+        IWithThroughput<ParentT>,
+        IWithChildResource<ParentT>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace definition allowing to set options.
+    /// </summary>
+    /// <typeparam name="ParentT">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IWithOptions<ParentT> :
+        HasOptions.Definition.IWithOptions<IWithAttach<ParentT>>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace definition allowing to set throughput.
+    /// </summary>
+    /// <typeparam name="Parent">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IWithThroughput<Parent> :
+        HasThroughputSettings.Definition.IWithThroughput<IWithAttach<Parent>>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace definition allowing to set child resources.
+    /// </summary>
+    /// <typeparam name="ParentT">The stage of the parent definition to return to after attaching this definition.</typeparam>
+    public interface IWithChildResource<ParentT>
+    {
+        /// <summary>
+        /// Defines a new Cassandra table.
+        /// </summary>
+        /// <param name="name">The name of the Cassandra table.</param>
+        /// <returns>The next stage of the definition.</returns>
+        CassandraTable.Definition.IBlank<IWithAttach<ParentT>> DefineNewCassandraTable(string name);
+    }
+}
---
+++ src/ResourceManagement/CosmosDB/Domain/CassandraKeyspace/Update/IUpdate.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Management.CosmosDB.Fluent.CassandraKeyspace.Update
+{
+    /// <summary>
+    /// The entirety of Cassandra Keyspace update as a part of parent cosmos db account update.
+    /// </summary>
+    public interface IUpdate :
+        Microsoft.Azure.Management.ResourceManager.Fluent.Core.ChildResourceActions.ISettable<Microsoft.Azure.Management.CosmosDB.Fluent.CosmosDBAccount.Update.IUpdate>,
+        IWithOptions,
+        IWithThroughput,
+        IWithChildResource
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace update allowing to set options.
+    /// </summary>
+    public interface IWithOptions :
+        HasOptions.Update.IWithOptions<IUpdate>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace update allowing to set throughput.
+    /// </summary>
+    public interface IWithThroughput :
+        HasThroughputSettings.Update.IWithThroughput<IUpdate>
+    {
+    }
+
+    /// <summary>
+    /// The stage of the Cassandra Keyspace update allowing to set child resources.
+    /// </summary>
+    public interface IWithChildResource
+    {
+        /// <summary>
+        /// Defines a new Cassandra table.
+        /// </summary>
+        /// <param name="name">The name of Cassandra table.</param>
+        /// <returns>The next stage of the update.</returns>
+        CassandraTable.Definition.IBlank<IUpdate> DefineNewCassandraTable(string name);
+
+        /// <summary>
+        /// Updates a Cassandra table.
+        /// </summary>
+        /// <param name="name">The name of the Cassandra table.</param>
+        /// <returns>The next stage of the update.</returns>
+        CassandraTable.Update.IUpdate UpdateCassandraTable(string name);
+
+        /// <summary>
+        /// Removes a Cassandra table.
+        /// </summary>
+        /// <param name="name">The name of the Cassandra table.</param>
+        /// <returns>The next stage of the update.</returns>
+        IUpdate WithoutCassandraTable(string name);
+    }
+}
