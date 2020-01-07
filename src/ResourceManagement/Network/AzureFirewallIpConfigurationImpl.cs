26ac176e0cf6aa72b05cdeae5d410c2737c3c7d9
Chuang Xie <chuxi@microsoft.com>
2020-01-07 18:48:46 +0800

    Network: add AzureFirewall implementations

---
+++ src/ResourceManagement/Network/AzureFirewallIpConfigurationImpl.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Management.Network.Fluent
+{
+    using Microsoft.Azure.Management.Network.Fluent.Models;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+    internal class AzureFirewallIpConfigurationImpl :
+        Wrapper<AzureFirewallIPConfigurationInner>,
+        IAzureFirewallIpConfiguration
+    {
+        internal AzureFirewallIpConfigurationImpl(AzureFirewallIPConfigurationInner inner)
+            : base(inner)
+        {
+        }
+
+        string IAzureFirewallIpConfiguration.PrivateIPAddress
+        {
+            get
+            {
+                return Inner.PrivateIPAddress;
+            }
+        }
+
+        string IAzureFirewallIpConfiguration.SubnetId
+        {
+            get
+            {
+                return Inner.Subnet == null ? null : Inner.Subnet.Id;
+            }
+        }
+
+        string IAzureFirewallIpConfiguration.PublicIPAddressId
+        {
+            get
+            {
+                return Inner.PublicIPAddress == null ? null : Inner.PublicIPAddress.Id;
+            }
+        }
+
+        ProvisioningState IAzureFirewallIpConfiguration.ProvisioningState
+        {
+            get
+            {
+                return Inner.ProvisioningState;
+            }
+        }
+
+        string IAzureFirewallIpConfiguration.Name
+        {
+            get
+            {
+                return Inner.Name;
+            }
+        }
+
+        string IAzureFirewallIpConfiguration.Etag
+        {
+            get
+            {
+                return Inner.Etag;
+            }
+        }
+
+        string IAzureFirewallIpConfiguration.Id
+        {
+            get
+            {
+                return Inner.Id;
+            }
+        }
+    }
+}
---
+++ src/ResourceManagement/Network/AzureFirewallNatRuleCollectionImpl.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Management.Network.Fluent
+{
+    using Microsoft.Azure.Management.Network.Fluent.Models;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+    using System.Collections.Generic;
+
+    internal class AzureFirewallNatRuleCollectionImpl :
+        Wrapper<AzureFirewallNatRuleCollectionInner>,
+        IAzureFirewallNatRuleCollection
+    {
+        internal AzureFirewallNatRuleCollectionImpl(AzureFirewallNatRuleCollectionInner inner)
+            : base(inner)
+        {
+        }
+
+        int? IAzureFirewallNatRuleCollection.Priority
+        {
+            get
+            {
+                return Inner.Priority;
+            }
+        }
+
+        AzureFirewallNatRCAction IAzureFirewallNatRuleCollection.Action
+        {
+            get
+            {
+                return Inner.Action;
+            }
+        }
+
+        IReadOnlyList<AzureFirewallNatRule> IAzureFirewallNatRuleCollection.Rules
+        {
+            get
+            {
+                List<AzureFirewallNatRule> rules = new List<AzureFirewallNatRule>();
+                if (Inner.Rules != null)
+                {
+                    rules = new List<AzureFirewallNatRule>();
+                }
+                return rules.AsReadOnly();
+            }
+        }
+
+        ProvisioningState IAzureFirewallNatRuleCollection.ProvisioningState
+        {
+            get
+            {
+                return Inner.ProvisioningState;
+            }
+        }
+
+        string IAzureFirewallNatRuleCollection.Name
+        {
+            get
+            {
+                return Inner.Name;
+            }
+        }
+
+        string IAzureFirewallNatRuleCollection.Etag
+        {
+            get
+            {
+                return Inner.Etag;
+            }
+        }
+
+        string IAzureFirewallNatRuleCollection.Id
+        {
+            get
+            {
+                return Inner.Id;
+            }
+        }
+    }
+}
