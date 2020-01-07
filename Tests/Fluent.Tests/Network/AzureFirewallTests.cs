06b11d6c6b9b686cf0a435898bd999bb0e8ced8f
Chuang Xie <chuxi@microsoft.com>
2020-01-07 18:51:14 +0800

    Network: add test cases and session records

---
+++ Tests/Fluent.Tests/Network/AzureFirewallTests.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Azure.Tests;
+using Fluent.Tests.Common;
+using Microsoft.Azure.Management.Network.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Xunit;
+
+namespace Fluent.Tests.Network
+{
+    public class AzureFirewall
+    {
+        private static Region REGION = Region.AsiaSouthEast;
+
+        [Fact]
+        public void CanCreateUpdate()
+        {
+            using (var context = FluentMockContext.Start(GetType().FullName))
+            {
+                var rgName = SdkContext.RandomResourceName("rg", 10);
+                var fwName = SdkContext.RandomResourceName("azfw", 10);
+
+                var azure = TestHelper.CreateRollupClient();
+
+                try
+                {
+                    var resourceGroup = azure.ResourceGroups.Define(rgName)
+                        .WithRegion(REGION)
+                        .Create();
+
+                    var azureFirewall = azure.AzureFirewalls.Define(fwName)
+                        .WithExistingResourceGroup(resourceGroup)
+                        .WithRegion(REGION)
+                        .DefineAzureFirewallApplicationRuleCollection("App-Coll01")
+                            .DefineAzureFirewallApplicationRule("Allow-Google")
+                                .WithSourceAddress("10.0.2.0/24")
+                                .WithHttpProtocol(8080)
+                                .WithHttpsProtocol(8081)
+                                .WithTargetFullQualifiedDomainName("www.google.com")
+                                .Attach()
+                            .WithPriority(200)
+                            .WithAllowActionType()
+                            .Attach()
+                        .DefineAzureFirewallNetworkRuleCollection("RCNet01")
+                            .DefineAzureFirewallNetworkRule("Allow-DNS")
+                                .WithUdpProtocol()
+                                .WithSourceAddress("10.0.2.0/24")
+                                .WithDestinationAddress("209.244.0.3")
+                                .WithDestinationAddress("209.244.0.4")
+                                .WithDestinationPort("53")
+                                .Attach()
+                            .WithAllowActionType()
+                            .WithPriority(200)
+                            .Attach()
+                        .WithAzfwVnetSkuName()
+                        .WithAlertModeForThreatIntel()
+                        .Create();
+
+                    Assert.NotNull(azureFirewall);
+                    Assert.True(azureFirewall.ApplicationRuleCollections.Count == 1);
+                    Assert.Equal(AzureFirewallRCActionType.Allow, azureFirewall.ApplicationRuleCollections[0].Action.Type);
+                    Assert.True(azureFirewall.ApplicationRuleCollections[0].Rules.Count == 1);
+                    Assert.Equal("Allow-Google", azureFirewall.ApplicationRuleCollections[0].Rules[0].Name);
+                    Assert.Equal(AzureFirewallSkuName.AZFWVNet, azureFirewall.Sku.Name);
+                    Assert.Equal(AzureFirewallThreatIntelMode.Alert, azureFirewall.ThreatIntelMode);
+                    Assert.True(azureFirewall.AdditionalProperties.Count == 0);
+
+                    azureFirewall.Update()
+                        .WithDenyModeForThreatIntel()
+                        .WithAdditionalProperty("key1", "valueToAdd")
+                        .Apply();
+
+                    Assert.Equal(AzureFirewallThreatIntelMode.Deny, azureFirewall.ThreatIntelMode);
+                    Assert.True(azureFirewall.AdditionalProperties.Count == 1);
+                    Assert.True(azureFirewall.AdditionalProperties.ContainsKey("key1"));
+
+                }
+                finally
+                {
+                    try
+                    {
+                        azure.ResourceGroups.DeleteByName(rgName);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Fluent.Tests/Network/PrivateLinkServiceTests.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Azure.Tests;
+using Fluent.Tests.Common;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.Network.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Xunit;
+
+namespace Fluent.Tests.Network
+{
+    public class PrivateLinkService
+    {
+        private static Region REGION = Region.AsiaSouthEast;
+
+        [Fact]
+        public void CanCreateUpdate()
+        {
+            using (var context = FluentMockContext.Start(GetType().FullName))
+            {
+                var rgName = SdkContext.RandomResourceName("rg", 10);
+                var vnetName = SdkContext.RandomResourceName("vnet", 10);
+                var subnetName = SdkContext.RandomResourceName("subnet", 10);
+                var lbName = SdkContext.RandomResourceName("lb", 10);
+                var plsName = SdkContext.RandomResourceName("pls", 10);
+
+                var azure = TestHelper.CreateRollupClient();
+
+                try
+                {
+                    var resourceGroup = azure.ResourceGroups.Define(rgName)
+                        .WithRegion(REGION)
+                        .Create();
+
+                    var vnet = azure.Networks.Define(vnetName)
+                        .WithRegion(REGION)
+                        .WithExistingResourceGroup(resourceGroup)
+                        .WithAddressSpace("10.0.0.0/16")
+                        .WithSubnet(subnetName, "10.0.0.0/24")
+                        .Create();
+
+                    var loadBalancer = azure.LoadBalancers.Define(lbName)
+                        .WithRegion(REGION)
+                        .WithExistingResourceGroup(resourceGroup)
+                        .DefineLoadBalancingRule("myHttpRule")
+                            .WithProtocol(TransportProtocol.Tcp)
+                            .FromFrontend("myFrontEnd")
+                            .FromFrontendPort(80)
+                            .ToBackend("myBackEnd")
+                            .WithProbe("myProbe")
+                            .Attach()
+                        .DefinePrivateFrontend("myFrontEnd")
+                            .WithExistingSubnet(vnet, subnetName)
+                            .Attach()
+                        .DefineBackend("myBackEnd")
+                            .Attach()
+                        .DefineHttpProbe("myProbe")
+                            .WithRequestPath("/")
+                            .WithPort(80)
+                            .Attach()
+                        .WithSku(LoadBalancerSkuType.Standard)
+                        .Create();
+
+                    vnet.Subnets[subnetName].Inner.PrivateLinkServiceNetworkPolicies = "Disabled";
+                    vnet.Update()
+                        .UpdateSubnet(subnetName)
+                        .Parent()
+                        .Apply();
+
+                    ISubnet subnet = vnet.Subnets[subnetName];
+                    ILoadBalancerFrontend lbFrontend = loadBalancer.Frontends["myFrontEnd"];
+
+                    var privateLinkService = azure.PrivateLinkServices.Define(plsName)
+                        .WithExistingResourceGroup(resourceGroup)
+                        .WithRegion(REGION)
+                        .WithFrontendIpConfiguration(lbFrontend)
+                        .DefinePrivateLinkServiceIpConfiguration("myPrivateIPConfig")
+                            .SetAsPrimaryIpConfiguration()
+                            .WithSubnet(subnet.Inner.Id)
+                            .Attach()
+                        .Create();
+
+                    Assert.NotNull(privateLinkService);
+                    Assert.NotNull(privateLinkService.IpConfigurations);
+                    Assert.True(privateLinkService.IpConfigurations.Count == 1);
+                    Assert.False(privateLinkService.IsProxyProtocolEnabled);
+                    Assert.Equal(IPAllocationMethod.Dynamic, privateLinkService.IpConfigurations[0].PrivateIPAllocationMethod);
+
+                    privateLinkService.Update()
+                        .EnableProxyProtocol()
+                        .UpdatePrivateLinkServiceIpConfiguration("myPrivateIPConfig")
+                            .WithStaticPrivateIpAllocation()
+                            .WithPrivateIpAddress("10.0.0.5")
+                            .Attach()
+                        .Apply();
+
+                    Assert.True(privateLinkService.IsProxyProtocolEnabled);
+                    Assert.Equal(IPAllocationMethod.Static, privateLinkService.IpConfigurations[0].PrivateIPAllocationMethod);
+                }
+                finally
+                {
+                    try
+                    {
+                        azure.ResourceGroups.DeleteByName(rgName);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
