8dd236be501f0cfed3d23b3493b37faaeef476a0
Tanyi Chen <tanchen@microsoft.com>
2020-03-04 10:49:03 +0800

    feat: implement for index policy

--- src/ResourceManagement/CosmosDB/Domain/InterfaceImpl/IndexingPolicyImpl.cs
+++ src/ResourceManagement/CosmosDB/Domain/InterfaceImpl/IndexingPolicyImpl.cs
@@ -28,6 +28,11 @@ namespace Microsoft.Azure.Management.CosmosDB.Fluent
             return this.WithIncludedPath(includedPath);
         }
 
+        IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithIncludedPaths<DefinitionParentT>.WithIncludedPath(string path)
+        {
+            return this.WithIncludedPath(path);
+        }
+
         IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithExcludedPaths<DefinitionParentT>.WithExcludedPaths(IList<ExcludedPath> excludedPaths)
         {
             return this.WithExcludedPathsAppend(excludedPaths);
@@ -36,6 +41,11 @@ namespace Microsoft.Azure.Management.CosmosDB.Fluent
         IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithExcludedPaths<DefinitionParentT>.WithExcludedPath(ExcludedPath excludedPath)
         {
             return this.WithExcludedPath(excludedPath);
+        }
+
+        IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithExcludedPaths<DefinitionParentT>.WithExcludedPath(string path)
+        {
+            return this.WithExcludedPath(path);
         }
 
         IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithCompositeIndexes<DefinitionParentT>.WithCompositeIndexes(IList<IList<CompositePath>> compositePaths)
@@ -48,6 +58,26 @@ namespace Microsoft.Azure.Management.CosmosDB.Fluent
             return this.WithCompositeIndex(compositePath);
         }
 
+        IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT> IndexingPolicy.Definition.IWithCompositeIndexes<DefinitionParentT>.WithCompositeIndexEmptyList()
+        {
+            return this.WithCompositeIndexEmptyList();
+        }
+
+        IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT> IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT>.WithCompositePath(CompositePath compositePath)
+        {
+            return this.WithCompositePath(compositePath);
+        }
+
+        IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT> IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT>.WithCompositePath(string path, CompositePathSortOrder order)
+        {
+            return this.WithCompositePath(path, order);
+        }
+
+        IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithCompositeIndexList<DefinitionParentT>.Attach()
+        {
+            return this.AttachSelf();
+        }
+
         IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithSpatialIndexes<DefinitionParentT>.WithSpatialIndexes(IList<SpatialSpec> spatialSpecs)
         {
             return this.WithSpatialIndexesAppend(spatialSpecs);
@@ -58,112 +88,152 @@ namespace Microsoft.Azure.Management.CosmosDB.Fluent
             return this.WithSpatialIndex(spatialSpec);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithAutomatic<UpdateParentT>.WithAutomatic(bool isAutomatic)
+        IndexingPolicy.Definition.IWithAttach<DefinitionParentT> IndexingPolicy.Definition.IWithSpatialIndexes<DefinitionParentT>.WithSpatialIndex(string path, params SpatialType[] types)
+        {
+            return this.WithSpatialIndex(path, types);
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithAutomatic<UpdateParentT>.WithAutomatic(bool isAutomatic)
         {
             return this.WithAutomatic(isAutomatic);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIndexingMode<UpdateParentT>.WithIndexingMode(IndexingMode indexingMode)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIndexingMode<UpdateParentT>.WithIndexingMode(IndexingMode indexingMode)
         {
             return this.WithIndexingMode(indexingMode);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIndexingMode<UpdateParentT>.WithoutIndexingMode()
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIndexingMode<UpdateParentT>.WithoutIndexingMode()
         {
             return this.WithoutIndexingMode();
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPathsAppend(IList<IncludedPath> includedPaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPathsAppend(IList<IncludedPath> includedPaths)
         {
             return this.WithIncludedPathsAppend(includedPaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPathsReplace(IList<IncludedPath> includedPaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPathsReplace(IList<IncludedPath> includedPaths)
         {
             return this.WithIncludedPathsReplace(includedPaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPath(IncludedPath includedPath)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPath(IncludedPath includedPath)
         {
             return this.WithIncludedPath(includedPath);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithoutIncludedPath(string path)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithIncludedPath(string path)
+        {
+            return this.WithIncludedPath(path);
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithoutIncludedPath(string path)
         {
             return this.WithoutIncludedPath(path);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithoutIncludedPaths()
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithIncludedPaths<UpdateParentT>.WithoutIncludedPaths()
         {
             return this.WithoutIncludedPaths();
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPathsAppend(IList<ExcludedPath> excludedPaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPathsAppend(IList<ExcludedPath> excludedPaths)
         {
             return this.WithExcludedPathsAppend(excludedPaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPathsReplace(IList<ExcludedPath> excludedPaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPathsReplace(IList<ExcludedPath> excludedPaths)
         {
             return this.WithExcludedPathsReplace(excludedPaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPath(ExcludedPath excludedPath)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPath(ExcludedPath excludedPath)
         {
             return this.WithExcludedPath(excludedPath);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithoutExcludedPath(string path)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithExcludedPath(string path)
+        {
+            return this.WithExcludedPath(path);
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithoutExcludedPath(string path)
         {
             return this.WithoutExcludedPath(path);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithoutExcludedPaths()
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithExcludedPaths<UpdateParentT>.WithoutExcludedPaths()
         {
             return this.WithoutExcludedPaths();
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndexesAppend(IList<IList<CompositePath>> compositePaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndexesAppend(IList<IList<CompositePath>> compositePaths)
         {
             return this.WithCompositeIndexesAppend(compositePaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndexesReplace(IList<IList<CompositePath>> compositePaths)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndexesReplace(IList<IList<CompositePath>> compositePaths)
         {
             return this.WithCompositeIndexesReplace(compositePaths);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndex(IList<CompositePath> compositePath)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndex(IList<CompositePath> compositePath)
         {
             return this.WithCompositeIndex(compositePath);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithoutCompositeIndexes()
+        IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithCompositeIndexEmptyList()
+        {
+            return this.WithCompositeIndexEmptyList();
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexes<UpdateParentT>.WithoutCompositeIndexes()
         {
             return this.WithoutCompositeIndexes();
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndexesAppend(IList<SpatialSpec> spatialSpecs)
+        IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT>.WithCompositePath(CompositePath compositePath)
+        {
+            return this.WithCompositePath(compositePath);
+        }
+
+        IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT>.WithCompositePath(string path, CompositePathSortOrder order)
+        {
+            return this.WithCompositePath(path, order);
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithCompositeIndexList<UpdateParentT>.Attach()
+        {
+            return this.AttachSelf();
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndexesAppend(IList<SpatialSpec> spatialSpecs)
         {
             return this.WithSpatialIndexesAppend(spatialSpecs);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndexesReplace(IList<SpatialSpec> spatialSpecs)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndexesReplace(IList<SpatialSpec> spatialSpecs)
         {
             return this.WithSpatialIndexesReplace(spatialSpecs);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndex(SpatialSpec spatialSpec)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndex(SpatialSpec spatialSpec)
         {
             return this.WithSpatialIndex(spatialSpec);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithoutSpatialIndex(string path)
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithSpatialIndex(string path, params SpatialType[] types)
+        {
+            return this.WithSpatialIndex(path, types);
+        }
+
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithoutSpatialIndex(string path)
         {
             return this.WithoutSpatialIndex(path);
         }
 
-        IndexingPolicy.Update.IUpdate<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithoutSpatialIndexes()
+        IndexingPolicy.Update.IWithAttach<UpdateParentT> IndexingPolicy.Update.IWithSpatialIndexes<UpdateParentT>.WithoutSpatialIndexes()
         {
             return this.WithoutSpatialIndexes();
         }
