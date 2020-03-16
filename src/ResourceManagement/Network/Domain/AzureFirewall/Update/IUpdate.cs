718e74c5b7d9afc81c8a56a94ca938f9745c5de5
xccc-msft <chuxi@microsoft.com>
2020-03-16 19:36:45 +0800

    Network: update interfaces

--- src/ResourceManagement/Network/Domain/AzureFirewall/Update/IUpdate.cs
+++ src/ResourceManagement/Network/Domain/AzureFirewall/Update/IUpdate.cs
@@ -25,6 +25,20 @@ namespace Microsoft.Azure.Management.Network.Fluent.AzureFirewall.Update
     }
 
     /// <summary>
+    /// The combined template containing all the settings that can be modified.
+    /// </summary>
+    public interface IUpdateCombined :
+        IUpdate,
+        IWithApplicationRuleCollectionSettings,
+        IWithApplicationRuleSettings,
+        IWithNatRuleCollectionSettings,
+        IWithNatRuleSettings,
+        IWithNetworkRuleCollectionSettings,
+        IWithNetworkRuleSettings
+    {
+    }
+
+    /// <summary>
     /// The stage of the Azure firewall update allowing to specify the operation mode of threat intelligence.
     /// </summary>
     public interface IWithThreatIntelMode
@@ -147,27 +161,66 @@ namespace Microsoft.Azure.Management.Network.Fluent.AzureFirewall.Update
         IUpdate WithoutZone(string zone);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the application rule collection.
+    /// </summary>
     public interface IWithApplicationRuleCollection
     {
+        /// <summary>
+        /// Sets the Azure firewall application rule collection.
+        /// </summary>
+        /// <param name="name">The name of application rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithApplicationRuleCollectionSettings DefineAzureFirewallApplicationRuleCollection(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall application rule collection.
+        /// </summary>
+        /// <param name="name">The name of application rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithApplicationRuleCollectionSettings UpdateAzureFirewallApplicationRuleCollection(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall application rule collection.
+        /// </summary>
+        /// <param name="name">The name of application rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IUpdate WithoutAzureFirewallApplicationRuleCollection(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the application rule collection settings.
+    /// </summary>
     public interface IWithApplicationRuleCollectionSettings :
         BaseRuleCollection.IWithPriority<IWithApplicationRuleCollectionSettings>,
         BaseRuleCollection.IWithRuleCollectionActionType<IWithApplicationRuleCollectionSettings>,
         ResourceManager.Fluent.Core.ChildResource.Update.IInUpdate<IUpdate>
     {
+        /// <summary>
+        /// Sets the Azure firewall application rule.
+        /// </summary>
+        /// <param name="name">The name of application rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithApplicationRuleSettings DefineAzureFirewallApplicationRule(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall application rule.
+        /// </summary>
+        /// <param name="name">The name of application rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithApplicationRuleSettings UpdateAzureFirewallApplicationRule(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall application rule.
+        /// </summary>
+        /// <param name="name">The name of application rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithApplicationRuleCollectionSettings WithoutAzureFirewallApplicationRule(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the application rule settings.
+    /// </summary>
     public interface IWithApplicationRuleSettings :
         BaseRule.IWithDescription<IWithApplicationRuleSettings>,
         BaseRule.IWithSourceAddress<IWithApplicationRuleSettings>,
@@ -179,27 +232,66 @@ namespace Microsoft.Azure.Management.Network.Fluent.AzureFirewall.Update
     {
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the nat rule collection.
+    /// </summary>
     public interface IWithNatRuleCollection
     {
+        /// <summary>
+        /// Sets the Azure firewall nat rule collection.
+        /// </summary>
+        /// <param name="name">The name of nat rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithNatRuleCollectionSettings DefineAzureFirewallNatRuleCollection(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall nat rule collection.
+        /// </summary>
+        /// <param name="name">The name of nat rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithNatRuleCollectionSettings UpdateAzureFirewallNatRuleCollection(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall nat rule collection.
+        /// </summary>
+        /// <param name="name">The name of nat rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IUpdate WithoutAzureFirewallNatRuleCollection(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the nat rule collection settings.
+    /// </summary>
     public interface IWithNatRuleCollectionSettings :
         BaseRuleCollection.IWithPriority<IWithNatRuleCollectionSettings>,
         BaseRuleCollection.IWithNatRuleCollectionActionType<IWithNatRuleCollectionSettings>,
         ResourceManager.Fluent.Core.ChildResource.Update.IInUpdate<IUpdate>
     {
+        /// <summary>
+        /// Sets the Azure firewall nat rule.
+        /// </summary>
+        /// <param name="name">The name of nat rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNatRuleSettings DefineAzureFirewallNatRule(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall nat rule.
+        /// </summary>
+        /// <param name="name">The name of nat rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNatRuleSettings UpdateAzureFirewallNatRule(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall nat rule.
+        /// </summary>
+        /// <param name="name">The name of nat rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNatRuleCollectionSettings WithoutAzureFirewallNatRule(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the nat rule settings.
+    /// </summary>
     public interface IWithNatRuleSettings :
         BaseRule.IWithDescription<IWithNatRuleSettings>,
         BaseRule.IWithSourceAddress<IWithNatRuleSettings>,
@@ -210,27 +302,66 @@ namespace Microsoft.Azure.Management.Network.Fluent.AzureFirewall.Update
     {
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the network rule collection.
+    /// </summary>
     public interface IWithNetworkRuleCollection
     {
+        /// <summary>
+        /// Sets the Azure firewall network rule collection.
+        /// </summary>
+        /// <param name="name">The name of network rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithNetworkRuleCollectionSettings DefineAzureFirewallNetworkRuleCollection(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall network rule collection.
+        /// </summary>
+        /// <param name="name">The name of network rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IWithNetworkRuleCollectionSettings UpdateAzureFirewallNetworkRuleCollection(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall network rule collection.
+        /// </summary>
+        /// <param name="name">The name of network rule collection.</param>
+        /// <return>The next stage of the update.</return>
         IUpdate WithoutAzureFirewallNetworkRuleCollection(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the network rule collection settings.
+    /// </summary>
     public interface IWithNetworkRuleCollectionSettings :
         BaseRuleCollection.IWithPriority<IWithNetworkRuleCollectionSettings>,
         BaseRuleCollection.IWithRuleCollectionActionType<IWithNetworkRuleCollectionSettings>,
         ResourceManager.Fluent.Core.ChildResource.Update.IInUpdate<IUpdate>
     {
+        /// <summary>
+        /// Sets the Azure firewall network rule.
+        /// </summary>
+        /// <param name="name">The name of network rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNetworkRuleSettings DefineAzureFirewallNetworkRule(string name);
 
+        /// <summary>
+        /// Updates the Azure firewall network rule.
+        /// </summary>
+        /// <param name="name">The name of network rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNetworkRuleSettings UpdateAzureFirewallNetworkRule(string name);
 
+        /// <summary>
+        /// Removes the Azure firewall network rule.
+        /// </summary>
+        /// <param name="name">The name of network rule.</param>
+        /// <return>The next stage of the update.</return>
         IWithNetworkRuleCollectionSettings WithoutAzureFirewallNetworkRule(string name);
     }
 
+    /// <summary>
+    /// The stage of the Azure firewall update allowing to specify the network rule settings.
+    /// </summary>
     public interface IWithNetworkRuleSettings :
         BaseRule.IWithDescription<IWithNetworkRuleSettings>,
         BaseRule.IWithRuleProtocol<IWithNetworkRuleSettings>,
